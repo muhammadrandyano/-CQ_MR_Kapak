be0800cd9390f85670b960a31e486503b018bc9d
muhammadrandyano <muhammadrandyyano@gmail.com>
2026-03-31 22:17:40 +0700

    menambahkan button untuk Latihan 1, 2, dan 3

--- PraktikumADO 1/Form1.cs
+++ PraktikumADO 1/Form1.cs
@@ -96,5 +96,23 @@ namespace PraktikumADO
                 MessageBox.Show(ex.Message);
             }
         }
+
+        private void btnLatihan1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+                string query = "SELECT COUNT(*) FROM Dosen";
+                cmd = new SqlCommand(query, conn);
+                int jumlah = (int)cmd.ExecuteScalar();
+                txtHasil.Text = "Jumlah Dosen: " + jumlah.ToString();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
