d1c377dcb934fc39a019df9854d5ec18afee0f5f
muhammadrandyano <muhammadrandyyano@gmail.com>
2026-03-31 21:48:33 +0700

    menambahkan button btnConnect

--- PraktikumADO 1/Form1.Designer.cs
+++ PraktikumADO 1/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -48,11 +49,21 @@
             this.textBox1.Size = new System.Drawing.Size(435, 22);
             this.textBox1.TabIndex = 1;
             // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(48, 59);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConnect);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -66,6 +77,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnConnect;
     }
 }
 
