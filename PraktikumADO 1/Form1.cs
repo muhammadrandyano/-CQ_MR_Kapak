12ed757a0be4ca8f1f73b7f7888a4c5e67719e51
muhammadrandyano <muhammadrandyyano@gmail.com>
2026-03-31 22:13:53 +0700

    implementasi fungsi btnConnect_Click untuk tes koneksi

--- PraktikumADO 1/Form1.cs
+++ PraktikumADO 1/Form1.cs
@@ -27,5 +27,20 @@ namespace PraktikumADO
                 @"Data Source=DESKTOP-RAM20FI\APRILIYA; Initial Catalog=DBAkademikADO; Integrated Security=True"
             );
         }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+                MessageBox.Show("Koneksi ke database berhasil");
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
     }
 }
